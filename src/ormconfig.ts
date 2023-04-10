4268b4027e76b041a69930ffdcc5240547a74920
Dmitry Shevtsov <mail@flyweb.kz>
2023-04-10 12:05:37 +0600

    changed orm config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,11 +2,11 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 
 const config: PostgresConnectionOptions = {
     type: 'postgres',
-    host: process.env.PGHOST,
-    port: Number(process.env.PGPORT),
-    username: process.env.PGUSER,
-    password: String(process.env.PGPASSWORD),
-    database: process.env.PGDATABASE,
+    host: 'user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    username: 'menyou-main-db-0a6f902cce65848ac',
+    password: 'YkA9sWZS53eaAE7PD4B6gX9z645FCP',
+    database: 'menyou-main-db-0a6f902cce65848ac',
     entities: [__dirname + '/**/*.entity.js'],
     synchronize: false,
     migrations: [__dirname + '/migrations/**/*{.ts, .js}']
